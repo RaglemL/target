f29eb3109744dc3fc8397e99373d5d184667b79a
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-04-30 12:59:16 -0600

    target.sh

--- target.sh
+++ target.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Create a new Google Cloud project
-gcloud projects create name-target --name="Name-Target"
+gcloud projects create finn-partners-target --name="Finn-Partners-Target"
 
 # Set the current Google Cloud project
-gcloud config set project name-target
+gcloud config set project finn-partners-target
 
 # Create a new service account
-gcloud iam service-accounts create Name-Target --project=name-target
+gcloud iam service-accounts create Finn-Partners-Target --project=finn-partners-target
 
 # Add IAM policy binding to the project
-gcloud projects add-iam-policy-binding name-target --member="serviceAccount:Name-Target@name-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding finn-partners-target --member="serviceAccount:Finn-Partners-Target@finn-partners-source.iam.gserviceaccount.com" --role="roles/owner"
 
 # Get the unique ID of the service account
-gcloud iam service-accounts describe Name-Target@name-target.iam.gserviceaccount.com --project=name-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Finn-Partners-Target@finn-partners-target.iam.gserviceaccount.com --project=finn-partners-target --format="value(uniqueId)"
 
 # Create a service account key and save it to a JSON file
-gcloud iam service-accounts keys create Name-Target.json --iam-account=Name-Target@name-target.iam.gserviceaccount.com --project=name-target
+gcloud iam service-accounts keys create Finn-Partners-Target.json --iam-account=Finn-Partners-Target@finn-partners-target.iam.gserviceaccount.com --project=finn-partners-target
 
 # Enable necessary Google services
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=name-target
+gcloud services enable admin.googleapis.com drive.googleapis.com gmail.googleapis.com calendar-json.googleapis.com people.googleapis.com tasks.googleapis.com forms.googleapis.com groupsmigration.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=finn-partners-target
 
 # Download the service account key JSON file
-cloudshell download Name-Target.json
+cloudshell download Finn-Partners-Target.json
