8574caa25271e18dba71b51bf60b3ae1c01b1d9e
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-05-26 01:03:29 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,13 +2,13 @@
 
 # Create a new Google Cloud project
 
-gcloud projects create paranguaracutirimicoro-target --name="Paranguaracutirimicoro-Target"
+gcloud projects create monobaboso-target --name="Monobaboso-Target"
 
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 
-gcloud config set project paranguaracutirimicoro-target
+gcloud config set project monobaboso-target
 
 sleep 5 # Adding a delay of 5 seconds
 
@@ -28,37 +28,37 @@ sleep 5 # Adding a delay of 5 seconds
 
 # Create a new service account
 
-gcloud iam service-accounts create Paranguaracutirimicoro-Target --project=paranguaracutirimicoro-target
+gcloud iam service-accounts create Monobaboso-Target --project=monobaboso-target
 
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 
-gcloud projects add-iam-policy-binding paranguaracutirimicoro-target --member="serviceAccount:Paranguaracutirimicoro-Target@paranguaracutirimicoro-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding monobaboso-target --member="serviceAccount:Monobaboso-Target@monobaboso-target.iam.gserviceaccount.com" --role="roles/editor"
 
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 
-gcloud iam service-accounts describe Paranguaracutirimicoro-Target@paranguaracutirimicoro-target.iam.gserviceaccount.com --project=paranguaracutirimicoro-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Monobaboso-Target@monobaboso-target.iam.gserviceaccount.com --project=monobaboso-target --format="value(uniqueId)"
 
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 
-gcloud iam service-accounts keys create Paranguaracutirimicoro-Target.json --iam-account=Paranguaracutirimicoro-Target@paranguaracutirimicoro-target.iam.gserviceaccount.com --project=paranguaracutirimicoro-target
+gcloud iam service-accounts keys create Monobaboso-Target.json --iam-account=Monobaboso-Target@monobaboso-target.iam.gserviceaccount.com --project=monobaboso-target
 
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=paranguaracutirimicoro-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=monobaboso-target
 
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 
-cloudshell download Paranguaracutirimicoro-Target.json
+cloudshell download Monobaboso-Target.json
 
 sleep 5 # Adding a delay of 5 seconds
 
