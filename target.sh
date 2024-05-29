67333f1345597fcc79b8d575dcec4b96b2830551
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-05-29 15:39:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create aibeio-target --name="Aibeio-Target"
+gcloud projects create redawning-target --name="Redawning-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project aibeio-target
+gcloud config set project redawning-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 225501211921 --member="user:cloudasta@aibe.io" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 210233693604 --member="user:cloudasta@redawning.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 225501211921 --member="user:cloudasta@aibe.io" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 210233693604 --member="user:cloudasta@redawning.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=225501211921
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=210233693604
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Aibeio-Target --project=aibeio-target
+gcloud iam service-accounts create Redawning-Target --project=redawning-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding aibeio-target --member="serviceAccount:Aibeio-Target@aibeio-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding redawning-target --member="serviceAccount:Redawning-Target@redawning-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Aibeio-Target@aibeio-target.iam.gserviceaccount.com --project=aibeio-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Redawning-Target@redawning-target.iam.gserviceaccount.com --project=redawning-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Aibeio-Target.json --iam-account=Aibeio-Target@aibeio-target.iam.gserviceaccount.com --project=aibeio-target
+gcloud iam service-accounts keys create Redawning-Target.json --iam-account=Redawning-Target@redawning-target.iam.gserviceaccount.com --project=redawning-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=aibeio-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=redawning-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Aibeio-Target.json
+cloudshell download Redawning-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=225501211921
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=210233693604
 sleep 5 # Adding a delay of 5 seconds
 
 
