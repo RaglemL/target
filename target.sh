c3299f834214b2aa1147e7cfdfd0c6ff156abb4a
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-10 21:00:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create redawning-target --name="Redawning-Target"
+gcloud projects create theinfinitereality-target --name="Theinfinitereality-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project redawning-target
+gcloud config set project theinfinitereality-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 210233693604 --member="user:cloudasta@redawning.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 546790382531 --member="user:cloudasta@theinfinitereality.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 210233693604 --member="user:cloudasta@redawning.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 546790382531 --member="user:cloudasta@theinfinitereality.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=210233693604
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=546790382531
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Redawning-Target --project=redawning-target
+gcloud iam service-accounts create Theinfinitereality-Target --project=theinfinitereality-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding redawning-target --member="serviceAccount:Redawning-Target@redawning-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding theinfinitereality-target --member="serviceAccount:Theinfinitereality-Target@theinfinitereality-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Redawning-Target@redawning-target.iam.gserviceaccount.com --project=redawning-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Theinfinitereality-Target@theinfinitereality-target.iam.gserviceaccount.com --project=theinfinitereality-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Redawning-Target.json --iam-account=Redawning-Target@redawning-target.iam.gserviceaccount.com --project=redawning-target
+gcloud iam service-accounts keys create Theinfinitereality-Target.json --iam-account=Theinfinitereality-Target@theinfinitereality-target.iam.gserviceaccount.com --project=theinfinitereality-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=redawning-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=theinfinitereality-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Redawning-Target.json
+cloudshell download Theinfinitereality-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=210233693604
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=546790382531
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
