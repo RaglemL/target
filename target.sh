de5ac47b07bbb123ff8d470f23ca2d5d6c608ad1
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-17 10:51:47 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create keyservco-target --name="Keyservco-Target"
+gcloud projects create supplypointemail-target --name="Supplypointemail-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project keyservco-target
+gcloud config set project supplypointemail-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 180739354719 --member="user:cloudasta@keyservco.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 243278845904 --member="user:cloudasta@supplypointemail.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 180739354719 --member="user:cloudasta@keyservco.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 243278845904 --member="user:cloudasta@supplypointemail.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=180739354719
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=243278845904
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Keyservco-Target --project=keyservco-target
+gcloud iam service-accounts create Supplypointemail-Target --project=supplypointemail-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding keyservco-target --member="serviceAccount:Keyservco-Target@keyservco-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding supplypointemail-target --member="serviceAccount:Supplypointemail-Target@supplypointemail-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Keyservco-Target@keyservco-target.iam.gserviceaccount.com --project=keyservco-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Supplypointemail-Target@supplypointemail-target.iam.gserviceaccount.com --project=supplypointemail-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Keyservco-Target.json --iam-account=Keyservco-Target@keyservco-target.iam.gserviceaccount.com --project=keyservco-target
+gcloud iam service-accounts keys create Supplypointemail-Target.json --iam-account=Supplypointemail-Target@supplypointemail-target.iam.gserviceaccount.com --project=supplypointemail-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=keyservco-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=supplypointemail-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Keyservco-Target.json
+cloudshell download Supplypointemail-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=180739354719
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=243278845904
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
