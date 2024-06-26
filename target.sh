c098db4747eb398f683fd0603ce318fbe72ec1b4
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-26 08:08:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create launchpad-home-group-target --name="Launchpad-Home-Group-Target"
+gcloud projects create golandscapemariposa-target --name="Golandscapemariposa-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project launchpad-home-group-target
+gcloud config set project golandscapemariposa-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 392107220860 --member="user:cloudasta@launchpadhomegroup.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 804931912461 --member="user:cloudasta@archive.golandscapemariposa.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 392107220860 --member="user:cloudasta@launchpadhomegroup.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 804931912461 --member="user:cloudasta@archive.golandscapemariposa.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=392107220860
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=804931912461
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Launchpad-Home-Group-Target --project=launchpad-home-group-target
+gcloud iam service-accounts create Golandscapemariposa-Target --project=golandscapemariposa-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding launchpad-home-group-target --member="serviceAccount:Launchpad-Home-Group-Target@launchpad-home-group-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding golandscapemariposa-target --member="serviceAccount:Golandscapemariposa-Target@golandscapemariposa-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Launchpad-Home-Group-Target@launchpad-home-group-target.iam.gserviceaccount.com --project=launchpad-home-group-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Golandscapemariposa-Target@golandscapemariposa-target.iam.gserviceaccount.com --project=golandscapemariposa-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Launchpad-Home-Group-Target.json --iam-account=Launchpad-Home-Group-Target@launchpad-home-group-target.iam.gserviceaccount.com --project=launchpad-home-group-target
+gcloud iam service-accounts keys create Golandscapemariposa-Target.json --iam-account=Golandscapemariposa-Target@golandscapemariposa-target.iam.gserviceaccount.com --project=golandscapemariposa-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=launchpad-home-group-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=golandscapemariposa-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Launchpad-Home-Group-Target.json
+cloudshell download Golandscapemariposa-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=392107220860
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=804931912461
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
