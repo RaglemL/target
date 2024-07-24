fdc0c79196787627c06df71160f734f7c8b8adc0
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-07-24 11:15:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,12 +2,12 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create lsdjfhkjdshfjkdsh-target --name="Lsdjfhkjdshfjkdsh-Target"
+gcloud projects create DRL-target --name="Drl-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project lsdjfhkjdshfjkdsh-target
+gcloud config set project DRL-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Lsdjfhkjdshfjkdsh-Target --project=lsdjfhkjdshfjkdsh-target
+gcloud iam service-accounts create Drl-Target --project=DRL-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding lsdjfhkjdshfjkdsh-target --member="serviceAccount:Lsdjfhkjdshfjkdsh-Target@lsdjfhkjdshfjkdsh-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding DRL-target --member="serviceAccount:Drl-Target@DRL-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Lsdjfhkjdshfjkdsh-Target@lsdjfhkjdshfjkdsh-target.iam.gserviceaccount.com --project=lsdjfhkjdshfjkdsh-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Drl-Target@DRL-target.iam.gserviceaccount.com --project=DRL-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Lsdjfhkjdshfjkdsh-Target.json --iam-account=Lsdjfhkjdshfjkdsh-Target@lsdjfhkjdshfjkdsh-target.iam.gserviceaccount.com --project=lsdjfhkjdshfjkdsh-target
+gcloud iam service-accounts keys create Drl-Target.json --iam-account=Drl-Target@DRL-target.iam.gserviceaccount.com --project=DRL-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=lsdjfhkjdshfjkdsh-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=DRL-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Lsdjfhkjdshfjkdsh-Target.json
+cloudshell download Drl-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
