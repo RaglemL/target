e20e459f28165c3255b96eda23c66f3d148b4966
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-07-24 11:18:52 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create domain.com-target --name="Domain.Com-Target"
+gcloud projects create membersy-target --name="Membersy-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project domain.com-target
+gcloud config set project membersy-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 6788713530 --member="user:cloudasta@slkdfglfkjdhgjfldh.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 6788713530 --member="user:cloudasta@membersy.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 6788713530 --member="user:cloudasta@slkdfglfkjdhgjfldh.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 6788713530 --member="user:cloudasta@membersy.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Domain.Com-Target --project=domain.com-target
+gcloud iam service-accounts create Membersy-Target --project=membersy-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding domain.com-target --member="serviceAccount:Domain.Com-Target@domain.com-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding membersy-target --member="serviceAccount:Membersy-Target@membersy-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Domain.Com-Target@domain.com-target.iam.gserviceaccount.com --project=domain.com-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Membersy-Target@membersy-target.iam.gserviceaccount.com --project=membersy-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Domain.Com-Target.json --iam-account=Domain.Com-Target@domain.com-target.iam.gserviceaccount.com --project=domain.com-target
+gcloud iam service-accounts keys create Membersy-Target.json --iam-account=Membersy-Target@membersy-target.iam.gserviceaccount.com --project=membersy-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=domain.com-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=membersy-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Domain.Com-Target.json
+cloudshell download Membersy-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
