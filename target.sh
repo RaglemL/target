5b3e09370fc8beeb155ec4a0d1dfb79e27aa0936
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-11-01 16:32:46 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create legalsoft-target --name="Legalsoft-Target"
+gcloud projects create legalsoft2-target --name="Legalsoft2-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project legalsoft-target
+gcloud config set project legalsoft2-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 669748195902 --member="user:migration@legalsoft.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 669748195902 --member="user:migration@legalsoft2.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 669748195902 --member="user:migration@legalsoft.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 669748195902 --member="user:migration@legalsoft2.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Legalsoft-Target --project=legalsoft-target
+gcloud iam service-accounts create Legalsoft2-Target --project=legalsoft2-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding legalsoft-target --member="serviceAccount:Legalsoft-Target@legalsoft-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding legalsoft2-target --member="serviceAccount:Legalsoft2-Target@legalsoft2-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Legalsoft-Target@legalsoft-target.iam.gserviceaccount.com --project=legalsoft-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Legalsoft2-Target@legalsoft2-target.iam.gserviceaccount.com --project=legalsoft2-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Legalsoft-Target.json --iam-account=Legalsoft-Target@legalsoft-target.iam.gserviceaccount.com --project=legalsoft-target
+gcloud iam service-accounts keys create Legalsoft2-Target.json --iam-account=Legalsoft2-Target@legalsoft2-target.iam.gserviceaccount.com --project=legalsoft2-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=legalsoft-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=legalsoft2-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Legalsoft-Target.json
+cloudshell download Legalsoft2-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
