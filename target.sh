9be76408775d9f46689116b39da261e0ea6a36a8
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-11-07 13:38:47 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create launchpad-homegroup-target --name="Launchpad-Homegroup-Target"
+gcloud projects create finnpartners-target --name="Finnpartners-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project launchpad-homegroup-target
+gcloud config set project finnpartners-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 392107220860 --member="user:cloudasta@launchpadhomegroup.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 235826488662 --member="user:cloudastaadmin@finnpartners.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 392107220860 --member="user:cloudasta@launchpadhomegroup.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 235826488662 --member="user:cloudastaadmin@finnpartners.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=392107220860
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=235826488662
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Launchpad-Homegroup-Target --project=launchpad-homegroup-target
+gcloud iam service-accounts create Finnpartners-Target --project=finnpartners-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding launchpad-homegroup-target --member="serviceAccount:Launchpad-Homegroup-Target@launchpad-homegroup-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding finnpartners-target --member="serviceAccount:Finnpartners-Target@finnpartners-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Launchpad-Homegroup-Target@launchpad-homegroup-target.iam.gserviceaccount.com --project=launchpad-homegroup-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Finnpartners-Target@finnpartners-target.iam.gserviceaccount.com --project=finnpartners-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Launchpad-Homegroup-Target.json --iam-account=Launchpad-Homegroup-Target@launchpad-homegroup-target.iam.gserviceaccount.com --project=launchpad-homegroup-target
+gcloud iam service-accounts keys create Finnpartners-Target.json --iam-account=Finnpartners-Target@finnpartners-target.iam.gserviceaccount.com --project=finnpartners-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=launchpad-homegroup-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=finnpartners-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Launchpad-Homegroup-Target.json
+cloudshell download Finnpartners-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=392107220860
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=235826488662
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
