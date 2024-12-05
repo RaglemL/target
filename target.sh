b0eb7eee3d2865e13cd614b0c6da14c86faa74b9
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-12-04 19:50:46 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create majorityagency-target --name="Majorityagency-Target"
+gcloud projects create archive.bevelpr-target --name="Archive.Bevelpr-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project majorityagency-target
+gcloud config set project archive.bevelpr-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 94156313500 --member="user:cloudasta@majorityagency.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 612762871216 --member="user:cloudasta@archive.bevelpr.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 94156313500 --member="user:cloudasta@majorityagency.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 612762871216 --member="user:cloudasta@archive.bevelpr.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=94156313500
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=612762871216
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Majorityagency-Target --project=majorityagency-target
+gcloud iam service-accounts create Archive.Bevelpr-Target --project=archive.bevelpr-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding majorityagency-target --member="serviceAccount:Majorityagency-Target@majorityagency-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding archive.bevelpr-target --member="serviceAccount:Archive.Bevelpr-Target@archive.bevelpr-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Majorityagency-Target@majorityagency-target.iam.gserviceaccount.com --project=majorityagency-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Archive.Bevelpr-Target@archive.bevelpr-target.iam.gserviceaccount.com --project=archive.bevelpr-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Majorityagency-Target.json --iam-account=Majorityagency-Target@majorityagency-target.iam.gserviceaccount.com --project=majorityagency-target
+gcloud iam service-accounts keys create Archive.Bevelpr-Target.json --iam-account=Archive.Bevelpr-Target@archive.bevelpr-target.iam.gserviceaccount.com --project=archive.bevelpr-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=majorityagency-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=archive.bevelpr-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Majorityagency-Target.json
+cloudshell download Archive.Bevelpr-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=94156313500
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=612762871216
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
