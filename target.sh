cff2ba379ff9cc67f0923879e1b6883336edde27
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-02-13 19:28:49 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create foundationsmgmt-target --name="Foundationsmgmt-Target"
+gcloud projects create pelamela-target --name="Pelamela-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project foundationsmgmt-target
+gcloud config set project pelamela-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 1041936176564 --member="user:cloudasta@foundationsmgmt.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 1041936176564 --member="user:cloudasta@pelamela.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 1041936176564 --member="user:cloudasta@foundationsmgmt.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 1041936176564 --member="user:cloudasta@pelamela.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Foundationsmgmt-Target --project=foundationsmgmt-target
+gcloud iam service-accounts create Pelamela-Target --project=pelamela-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding foundationsmgmt-target --member="serviceAccount:Foundationsmgmt-Target@foundationsmgmt-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding pelamela-target --member="serviceAccount:Pelamela-Target@pelamela-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Foundationsmgmt-Target@foundationsmgmt-target.iam.gserviceaccount.com --project=foundationsmgmt-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Pelamela-Target@pelamela-target.iam.gserviceaccount.com --project=pelamela-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Foundationsmgmt-Target.json --iam-account=Foundationsmgmt-Target@foundationsmgmt-target.iam.gserviceaccount.com --project=foundationsmgmt-target
+gcloud iam service-accounts keys create Pelamela-Target.json --iam-account=Pelamela-Target@pelamela-target.iam.gserviceaccount.com --project=pelamela-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=foundationsmgmt-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=pelamela-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Foundationsmgmt-Target.json
+cloudshell download Pelamela-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
