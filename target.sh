95178fa3aa4ee77a4333f846fb1805ec19fb10a3
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-03-07 10:33:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create nesti-target --name="Nesti-Target"
+gcloud projects create arbor-insight-target --name="Arbor-Insight-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project nesti-target
+gcloud config set project arbor-insight-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 364919420141 --member="user:cloudasta@nesti.io" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 381786240010 --member="user:cloudasta@arbor-insight.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 364919420141 --member="user:cloudasta@nesti.io" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 381786240010 --member="user:cloudasta@arbor-insight.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=364919420141
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=381786240010
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Nesti-Target --project=nesti-target
+gcloud iam service-accounts create Arbor-Insight-Target --project=arbor-insight-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding nesti-target --member="serviceAccount:Nesti-Target@nesti-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding arbor-insight-target --member="serviceAccount:Arbor-Insight-Target@arbor-insight-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Nesti-Target@nesti-target.iam.gserviceaccount.com --project=nesti-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Arbor-Insight-Target@arbor-insight-target.iam.gserviceaccount.com --project=arbor-insight-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Nesti-Target.json --iam-account=Nesti-Target@nesti-target.iam.gserviceaccount.com --project=nesti-target
+gcloud iam service-accounts keys create Arbor-Insight-Target.json --iam-account=Arbor-Insight-Target@arbor-insight-target.iam.gserviceaccount.com --project=arbor-insight-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=nesti-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=arbor-insight-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Nesti-Target.json
+cloudshell download Arbor-Insight-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=364919420141
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=381786240010
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
