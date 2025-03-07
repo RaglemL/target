3b688fde75abda2d79640b8b36aa4b38315fcd9a
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-03-07 12:42:46 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create arbor-insight-target --name="Arbor-Insight-Target"
+gcloud projects create vapeclubsv-target --name="Vapeclubsv-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project arbor-insight-target
+gcloud config set project vapeclubsv-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 381786240010 --member="user:cloudasta@arbor-insight.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 1090329255832 --member="user:paola@vapeclubsv.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 381786240010 --member="user:cloudasta@arbor-insight.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 1090329255832 --member="user:paola@vapeclubsv.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=381786240010
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=1090329255832
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Arbor-Insight-Target --project=arbor-insight-target
+gcloud iam service-accounts create Vapeclubsv-Target --project=vapeclubsv-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding arbor-insight-target --member="serviceAccount:Arbor-Insight-Target@arbor-insight-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding vapeclubsv-target --member="serviceAccount:Vapeclubsv-Target@vapeclubsv-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Arbor-Insight-Target@arbor-insight-target.iam.gserviceaccount.com --project=arbor-insight-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Vapeclubsv-Target@vapeclubsv-target.iam.gserviceaccount.com --project=vapeclubsv-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Arbor-Insight-Target.json --iam-account=Arbor-Insight-Target@arbor-insight-target.iam.gserviceaccount.com --project=arbor-insight-target
+gcloud iam service-accounts keys create Vapeclubsv-Target.json --iam-account=Vapeclubsv-Target@vapeclubsv-target.iam.gserviceaccount.com --project=vapeclubsv-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=arbor-insight-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=vapeclubsv-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Arbor-Insight-Target.json
+cloudshell download Vapeclubsv-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=381786240010
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=1090329255832
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
