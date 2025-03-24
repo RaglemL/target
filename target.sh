bc91ef97a3efe812f2b3e2499449ac87a8254923
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-03-24 11:30:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create sunnyroad-target --name="Sunnyroad-Target"
+gcloud projects create launchpadhomegroup-target --name="Launchpadhomegroup-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project sunnyroad-target
+gcloud config set project launchpadhomegroup-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 1059554513327 --member="user:cloudasta@sunnyroad.co" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 392107220860 --member="user:cloudasta@launchpadhomegroup.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 1059554513327 --member="user:cloudasta@sunnyroad.co" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 392107220860 --member="user:cloudasta@launchpadhomegroup.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=1059554513327
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=392107220860
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Sunnyroad-Target --project=sunnyroad-target
+gcloud iam service-accounts create Launchpadhomegroup-Target --project=launchpadhomegroup-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding sunnyroad-target --member="serviceAccount:Sunnyroad-Target@sunnyroad-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding launchpadhomegroup-target --member="serviceAccount:Launchpadhomegroup-Target@launchpadhomegroup-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Sunnyroad-Target@sunnyroad-target.iam.gserviceaccount.com --project=sunnyroad-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Launchpadhomegroup-Target@launchpadhomegroup-target.iam.gserviceaccount.com --project=launchpadhomegroup-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Sunnyroad-Target.json --iam-account=Sunnyroad-Target@sunnyroad-target.iam.gserviceaccount.com --project=sunnyroad-target
+gcloud iam service-accounts keys create Launchpadhomegroup-Target.json --iam-account=Launchpadhomegroup-Target@launchpadhomegroup-target.iam.gserviceaccount.com --project=launchpadhomegroup-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=sunnyroad-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=launchpadhomegroup-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Sunnyroad-Target.json
+cloudshell download Launchpadhomegroup-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=1059554513327
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=392107220860
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
