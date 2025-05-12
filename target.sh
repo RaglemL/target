a8cdc080ab9d33f1aeb17fc3b21c536d6c66740a
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-05-12 11:39:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,12 +2,12 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create temp.seated-target --name="Temp.Seated-Target"
+gcloud projects create temp-seated-target --name="Temp-Seated-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project temp.seated-target
+gcloud config set project temp-seated-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Temp.Seated-Target --project=temp.seated-target
+gcloud iam service-accounts create Temp-Seated-Target --project=temp-seated-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding temp.seated-target --member="serviceAccount:Temp.Seated-Target@temp.seated-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding temp-seated-target --member="serviceAccount:Temp-Seated-Target@temp-seated-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Temp.Seated-Target@temp.seated-target.iam.gserviceaccount.com --project=temp.seated-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Temp-Seated-Target@temp-seated-target.iam.gserviceaccount.com --project=temp-seated-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Temp.Seated-Target.json --iam-account=Temp.Seated-Target@temp.seated-target.iam.gserviceaccount.com --project=temp.seated-target
+gcloud iam service-accounts keys create Temp-Seated-Target.json --iam-account=Temp-Seated-Target@temp-seated-target.iam.gserviceaccount.com --project=temp-seated-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=temp.seated-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=temp-seated-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Temp.Seated-Target.json
+cloudshell download Temp-Seated-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
