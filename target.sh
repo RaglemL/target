f84303e426c9965007f4f2f381f14d77832d2551
RaglemL <133904396+RaglemL@users.noreply.github.com>
2026-02-20 16:20:51 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create thrivelaundry-target --name="Thrivelaundry-Target"
+gcloud projects create securecapitalsolutions-target --name="Securecapitalsolutions-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project thrivelaundry-target
+gcloud config set project securecapitalsolutions-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 550421272648 --member="user:cloudasta@thrivelaundry.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 749303432061 --member="user:cloudasta@securecapitalsolutions.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 550421272648 --member="user:cloudasta@thrivelaundry.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 749303432061 --member="user:cloudasta@securecapitalsolutions.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=550421272648
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=749303432061
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Thrivelaundry-Target --project=thrivelaundry-target
+gcloud iam service-accounts create Securecapitalsolutions-Target --project=securecapitalsolutions-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding thrivelaundry-target --member="serviceAccount:Thrivelaundry-Target@thrivelaundry-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding securecapitalsolutions-target --member="serviceAccount:Securecapitalsolutions-Target@securecapitalsolutions-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Thrivelaundry-Target@thrivelaundry-target.iam.gserviceaccount.com --project=thrivelaundry-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Securecapitalsolutions-Target@securecapitalsolutions-target.iam.gserviceaccount.com --project=securecapitalsolutions-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Thrivelaundry-Target.json --iam-account=Thrivelaundry-Target@thrivelaundry-target.iam.gserviceaccount.com --project=thrivelaundry-target
+gcloud iam service-accounts keys create Securecapitalsolutions-Target.json --iam-account=Securecapitalsolutions-Target@securecapitalsolutions-target.iam.gserviceaccount.com --project=securecapitalsolutions-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=thrivelaundry-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=securecapitalsolutions-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Thrivelaundry-Target.json
+cloudshell download Securecapitalsolutions-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=550421272648
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=749303432061
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
