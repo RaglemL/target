7a2c6582f27aa4910d624c483c4cbcea1e7b051c
RaglemL <133904396+RaglemL@users.noreply.github.com>
2026-04-30 10:49:47 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create heartlandsignal-target --name="Heartlandsignal-Target"
+gcloud projects create industrialreports-target --name="Industrialreports-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project heartlandsignal-target
+gcloud config set project industrialreports-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 185184541132 --member="user:cloudasta@heartlandsignal.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 508788400083 --member="user:cloudasta@industrialreports.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 185184541132 --member="user:cloudasta@heartlandsignal.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 508788400083 --member="user:cloudasta@industrialreports.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=185184541132
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=508788400083
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Heartlandsignal-Target --project=heartlandsignal-target
+gcloud iam service-accounts create Industrialreports-Target --project=industrialreports-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding heartlandsignal-target --member="serviceAccount:Heartlandsignal-Target@heartlandsignal-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding industrialreports-target --member="serviceAccount:Industrialreports-Target@industrialreports-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Heartlandsignal-Target@heartlandsignal-target.iam.gserviceaccount.com --project=heartlandsignal-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Industrialreports-Target@industrialreports-target.iam.gserviceaccount.com --project=industrialreports-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Heartlandsignal-Target.json --iam-account=Heartlandsignal-Target@heartlandsignal-target.iam.gserviceaccount.com --project=heartlandsignal-target
+gcloud iam service-accounts keys create Industrialreports-Target.json --iam-account=Industrialreports-Target@industrialreports-target.iam.gserviceaccount.com --project=industrialreports-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=heartlandsignal-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=industrialreports-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Heartlandsignal-Target.json
+cloudshell download Industrialreports-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=185184541132
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=508788400083
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
