819c85378d92ca1f92e1156b7c1f800d26d55b85
RaglemL <133904396+RaglemL@users.noreply.github.com>
2026-06-12 10:47:45 -0600

    Automated update from Google Sheets

--- target.sh
+++ target.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create solartechnologies-target --name="Solartechnologies-Target"
+gcloud projects create sacredhour-target --name="Sacredhour-Target"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project solartechnologies-target
+gcloud config set project sacredhour-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 508265238618 --member="user:cloudasta@solartechnologies.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 814548624939 --member="user:cloudasta@sacredhour.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 508265238618 --member="user:cloudasta@solartechnologies.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 814548624939 --member="user:cloudasta@sacredhour.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=508265238618
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=814548624939
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Solartechnologies-Target --project=solartechnologies-target
+gcloud iam service-accounts create Sacredhour-Target --project=sacredhour-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding solartechnologies-target --member="serviceAccount:Solartechnologies-Target@solartechnologies-target.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding sacredhour-target --member="serviceAccount:Sacredhour-Target@sacredhour-target.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Solartechnologies-Target@solartechnologies-target.iam.gserviceaccount.com --project=solartechnologies-target --format="value(uniqueId)"
+gcloud iam service-accounts describe Sacredhour-Target@sacredhour-target.iam.gserviceaccount.com --project=sacredhour-target --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Solartechnologies-Target.json --iam-account=Solartechnologies-Target@solartechnologies-target.iam.gserviceaccount.com --project=solartechnologies-target
+gcloud iam service-accounts keys create Sacredhour-Target.json --iam-account=Sacredhour-Target@sacredhour-target.iam.gserviceaccount.com --project=sacredhour-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=solartechnologies-target
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=sacredhour-target
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Solartechnologies-Target.json
+cloudshell download Sacredhour-Target.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=508265238618
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=814548624939
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
